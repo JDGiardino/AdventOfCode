1c59ef2832bd5a92ebb72ee119eda0f0d698e9bb
Joe Giardino <JDGiardino@gmail.com>
2022-12-07 00:05:11 -0500

    Day 3 Puzzle (Part 1)

---
+++ 2022/day3/day3.py
@@ -0,0 +1,45 @@
+from string import ascii_lowercase, ascii_uppercase
+
+
+def get_dict_key(character_numbers_dictionary, character):
+    for key, value in character_numbers_dictionary.items():
+        if character == value:
+            return key
+
+
+def same_character_in_string(string1, string2):
+    for character in string1:
+        if character in string2:
+            return character
+
+
+def string_into_two(string):
+    string1 = string[:len(string) // 2]
+    string2 = string[len(string) // 2:]
+    return string1, string2
+
+
+def character_numbers():
+    character_numbers_dictionary = {}
+    number = 1
+    all_characters = ascii_lowercase + ascii_uppercase  # These are special strings that include every character
+    for character in all_characters:
+        character_numbers_dictionary.update({number: character})
+        number += 1
+    return character_numbers_dictionary
+
+
+def main():
+    input_data = open("input.txt", "r")
+    character_numbers_dictionary = character_numbers()
+    total_numbers = 0
+    for string in input_data:
+        string1, string2 = string_into_two(string)
+        character = same_character_in_string(string1, string2)
+        number = get_dict_key(character_numbers_dictionary, character)
+        total_numbers += number
+    print(f"The sum of the priorities of items that appear in both compartments of each ruksack is {total_numbers}")
+
+
+if __name__ == "__main__":
+    main()
